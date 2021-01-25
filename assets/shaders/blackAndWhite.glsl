b6dbfac8698480635f3ae4538743c6a31b05aa9d
arian <arianyadegarnia165@gmail.com>
2021-01-25 21:00:42 +0330

    added blackAndWhite shader

---
+++ assets/shaders/blackAndWhite.glsl
@@ -0,0 +1,31 @@
+#type vertex
+#version 330 core
+layout (location=0) in vec3 aPos;
+layout (location=1) in vec4 aColor;
+
+uniform mat4 uProjection; // uniform sticks around from object to object
+uniform mat4 uView;
+
+out vec4 fColor; // passes fColor to fragment
+
+
+void main()
+{
+    fColor = aColor;
+    gl_Position = uProjection * uView * vec4(aPos, 1.0);
+}
+
+#type fragment
+#version 330 core
+
+uniform float uTime;
+
+in vec4 fColor; // gets the out fColor from above
+
+out vec4 color;
+
+void main()
+{
+    float avg = (fColor.r + fColor.g + fColor.b) / 3;
+    color = vec4(avg, avg, avg, 1);
+}
