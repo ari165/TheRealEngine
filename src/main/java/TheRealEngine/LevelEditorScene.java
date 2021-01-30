04ad2f9657e80d7e2778ce689d6de0cd33154fee
arian <arianyadegarnia165@gmail.com>
2021-01-30 16:56:16 +0330

    added the ability to change a gameObject's sprite and color

--- src/main/java/TheRealEngine/LevelEditorScene.java
+++ src/main/java/TheRealEngine/LevelEditorScene.java
@@ -8,6 +8,8 @@ import util.AssetPool;
 
 public class LevelEditorScene extends Scene{
 
+    private GameObject obj1;
+    Spritesheet sprites;
     public LevelEditorScene(){
 
     }
@@ -18,9 +20,9 @@ public class LevelEditorScene extends Scene{
 
         this.camera = new Camera(new Vector2f(-250, 0));
 
-        Spritesheet sprites = AssetPool.getSpritesheet(Static_Strings.spritesheetPath);
+        sprites = AssetPool.getSpritesheet(Static_Strings.spritesheetPath);
 
-        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
@@ -37,8 +39,20 @@ public class LevelEditorScene extends Scene{
                 16, 16, 26, 0));
     }
 
+    private int spriteIndex = 0;
+    private float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
     @Override
     public void update(float dt) {
+        spriteFlipTimeLeft -= dt;
+        if (spriteFlipTimeLeft <= 0){
+           spriteFlipTimeLeft = spriteFlipTime;
+           spriteIndex++;
+           if (spriteIndex > 4){
+               spriteIndex = 0;
+           }
+           obj1.getComponent(SpriteRenderer.class).SetSprite(sprites.getSprite(spriteIndex));
+        }
 
         for (GameObject go : this.gameObjects){
             go.update(dt);
