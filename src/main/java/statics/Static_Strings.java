50105f6ec20027adf760410beb2bad5904e64d7a
Arian <arianyadegarnia165@gmail.com>
2021-02-07 18:19:04 +0330

    changed the Static_Strings to be more flexible

--- src/main/java/statics/Static_Strings.java
+++ src/main/java/statics/Static_Strings.java
@@ -1,11 +1,14 @@
 package statics;
 
 public class Static_Strings {
-    public static String defaultShaderPath = "assets/shaders/default.glsl";
-    public static String noiseShaderPath = "assets/shaders/Noise.glsl";
+    public static String shaderPath = "assets/shaders/";
+    public static String texturePath = "assets/images/";
 
-    public static String testTexture1Path = "assets/images/apartment.png";
-    public static String testTexture2Path = "assets/images/TestSprite.png";
-    public static String spritesheetPath = "assets/images/spritesheet.png";
+    public static String defaultShaderPath = shaderPath + "default.glsl";
+    public static String noiseShaderPath = shaderPath + "Noise.glsl";
+
+    public static String testTexture1Path = texturePath + "apartment.png";
+    public static String testTexture2Path = texturePath + "TestSprite.png";
+    public static String spritesheetPath = texturePath + "spritesheet.png";
 
 }
