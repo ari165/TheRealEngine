d7a59c5ba8057e709869b092ccdcafc9fb0443d6
Arian <arianyadegarnia165@gmail.com>
2021-02-17 22:31:36 +0330

    added the base foundation of physics engine

---
+++ src/main/java/physics2D/primitives/Collider2D.java
@@ -0,0 +1,11 @@
+package physics2D.primitives;
+
+import components.Component;
+import org.joml.Vector2f;
+
+public class Collider2D extends Component {
+    protected Vector2f offset = new Vector2f();
+
+    //public abstract float getInertiaTensor(float mass)
+
+}
---
+++ src/main/java/physics2D/rigidbody/IntersectionDetector2D.java
@@ -0,0 +1,12 @@
+package physics2D.rigidbody;
+
+public class IntersectionDetector2D {
+    // ==========================
+    // Point vs. Primitive Tests
+    // ==========================
+
+
+    // ==========================
+    // Line vs. Primitive Tests
+    // ==========================
+}
