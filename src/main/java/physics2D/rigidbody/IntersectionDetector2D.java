957af85ffb975d9c3c9e61a9b758d3e297a22297
Arian <arianyadegarnia165@gmail.com>
2021-02-21 22:30:53 +0330

    line vs. circle intersection and unit test

--- src/main/java/physics2D/rigidbody/IntersectionDetector2D.java
+++ src/main/java/physics2D/rigidbody/IntersectionDetector2D.java
@@ -8,9 +8,9 @@ import renderer.Line2D;
 import util.Matha;
 
 public class IntersectionDetector2D {
-    // ==========================
+    // =========================
     // Point vs. Primitive Tests
-    // ==========================
+    // =========================
     public static boolean pointOnLine(Vector2f point, Line2D line){
         // Variables
         // dy: delta y, dx: delta x
@@ -24,6 +24,10 @@ public class IntersectionDetector2D {
 
         float dy = line.getEnd().y - line.getStart().y;
         float dx = line.getEnd().x - line.getStart().x;
+
+        if (dx == 0f){
+            return Matha.compare(point.x, line.getStart().x);
+        }
         float m = dy / dx;
 
         float b = line.getEnd().y - (m * line.getEnd().x);
@@ -32,7 +36,7 @@ public class IntersectionDetector2D {
         return point.y == m * point.x + b;
     }
 
-    public static boolean PointInCircle(Vector2f point, Circle circle){
+    public static boolean pointInCircle(Vector2f point, Circle circle){
         Vector2f circleCenter = circle.getCenter();
         Vector2f centerToPoint = new Vector2f(point).sub(circleCenter);
 
@@ -40,7 +44,7 @@ public class IntersectionDetector2D {
         return centerToPoint.lengthSquared() <= circle.getRadius() * circle.getRadius();
     }
 
-    public static boolean PointInAABB(Vector2f point, AABB box){
+    public static boolean pointInAABB(Vector2f point, AABB box){
         Vector2f min = box.getMin();
         Vector2f max = box.getMax();
 
@@ -48,7 +52,7 @@ public class IntersectionDetector2D {
                 point.y <= max.y && point.y >= min.y;
     }
 
-    public static boolean PointInBox2D(Vector2f point, Box2D box){
+    public static boolean pointInBox2D(Vector2f point, Box2D box){
         // Translate point to local space
         Vector2f pointLocalBox = new Vector2f(point);
         Matha.rotate(pointLocalBox, box.getRigidbody().getRotation(), box.getRigidbody().getPosition());
@@ -60,7 +64,39 @@ public class IntersectionDetector2D {
                 pointLocalBox.y <= max.y && pointLocalBox.y >= min.y;
     }
 
-    // ==========================
+    // ========================
     // Line vs. Primitive Tests
-    // ==========================
+    // ========================
+
+    public static boolean lineAndCircle(Line2D line, Circle circle){
+        // Formulas
+        // A.B = |A||B|cos(Theta)
+        // (A.B) / (B.B) = |A| / |B|
+
+        // if start or end of the line is in the circle, return true
+        if (pointInCircle(line.getStart(), circle) || pointInCircle(line.getEnd(), circle)){
+            return true;
+        }
+
+        Vector2f ab = new Vector2f(line.getEnd()).sub(line.getStart());
+
+        // project the point (circle position) onto ab (line segment)
+        // parameterized position t
+        Vector2f circleCenter = circle.getCenter();
+        Vector2f centerToLineStart = new Vector2f(circleCenter).sub(line.getStart());
+
+        // dot the vector (t is the percentage)
+        float t = centerToLineStart.dot(ab) / ab.dot(ab);
+
+        if (t < 0.0f || t > 1.0f){
+            // then it means its not on the line
+            return false;
+        }
+
+        // Find the closest point to line segment
+        Vector2f closestPoint = new Vector2f(line.getStart()).add(ab.mul(t));
+
+        return pointInCircle(closestPoint, circle);
+    }
+
 }
